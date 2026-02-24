bc2b87899b542a756a04772590c2c5c517e0d9b4
Artyom Evsyukov <artyom.evsyukov@gmail.com>
2026-02-24 12:59:03 +0300

    Slice pattern

--- src/model/coffeeStore.ts
+++ src/model/coffeeStore.ts
@@ -1,92 +1,20 @@
-import axios from "axios"
+import type { getCoffeeListReqParams } from "../types/storeTypes"
+import { create } from "zustand"
+import { devtools, persist } from "zustand/middleware"
 import type {
+  CartAction,
+  CartState,
   ListActions,
   ListState,
-  CoffeeType,
-  getCoffeeListReqParams,
-  OrderItem,
-  CartState,
-  CartAction,
-  OrderCoffeeRes,
 } from "../types/storeTypes"
-import { create, type StateCreator } from "zustand"
-import { devtools, persist } from "zustand/middleware"
-import { BASE_URL } from "../api/CoreApi"
-
-const initialState: ListState & CartState = {
-  coffeeList: undefined,
-  cart: [],
-  address: "",
-  params: { text: undefined },
-}
-
-const coffeeSlice: StateCreator<
-  ListState & ListActions & CartState & CartAction,
-  [["zustand/devtools", never], ["zustand/persist", unknown]]
-> = (set, get) => ({
-  ...initialState,
-
-  setParams: (newParams) => {
-    const { getCoffeeList, params } = get()
-    set({ params: { ...params, ...newParams } }, false, "setParams")
-    getCoffeeList(params)
-  },
-  getCoffeeList: async (params?: getCoffeeListReqParams) => {
-    try {
-      const { data } = await axios.get<CoffeeType[]>(BASE_URL, { params })
-      set({ coffeeList: data })
-    } catch (error) {
-      console.log(error)
-    }
-  },
-  orderCoffee: async () => {
-    const { cart, address, clearCart } = get()
-    try {
-      const { data } = await axios.post<OrderCoffeeRes>(`${BASE_URL}/order`, {
-        address,
-        orderItems: cart,
-      })
-      if (data.success) {
-        alert(data.message)
-        clearCart()
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  },
-  addToCart: (coffee: CoffeeType) => {
-    const { cart } = get()
-
-    const existingItem = cart.find((item) => item.id === coffee.id)
-    if (existingItem) {
-      const updateCart = cart.map((item) =>
-        item.id === coffee.id ? { ...item, quantity: item.quantity + 1 } : item,
-      )
-      set({ cart: updateCart })
-    } else {
-      const newItem: OrderItem = {
-        id: coffee.id,
-        name: `${coffee.name} ${coffee.subTitle}`,
-        size: "L",
-        quantity: 1,
-      }
-      // set({ cart: cart ? [...cart, newItem] : [newItem] })
-      set({ cart: [...cart, newItem] })
-    }
-  },
-  clearCart: () => {
-    set({ cart: [] })
-  },
-  setAddress: (address: string) => {
-    set({ address })
-  },
-})
+import { listSlice } from "./listSlice"
+import { cartSlice } from "./cartSlice"
 
 export const useCoffeeStore = create<
-  ListState & ListActions & CartState & CartAction
+  CartState & CartAction & ListActions & ListState
 >()(
   devtools(
-    persist(coffeeSlice, {
+    persist((...arg) => ({ ...listSlice(...arg), ...cartSlice(...arg) }), {
       name: "coffeeStore",
       partialize: (state) => ({ cart: state.cart, address: state.address }),
     }),
---
+++ src/model/listSlice.ts
@@ -0,0 +1,38 @@
+import type { StateCreator } from "zustand"
+import type {
+  CartAction,
+  CartState,
+  CoffeeType,
+  getCoffeeListReqParams,
+  ListActions,
+  ListState,
+} from "../types/storeTypes"
+import axios from "axios"
+import { BASE_URL } from "../api/CoreApi"
+
+const initialState: ListState = {
+  coffeeList: undefined,
+  params: { text: undefined },
+}
+
+export const listSlice: StateCreator<
+  CartState & CartAction & ListActions & ListState,
+  [["zustand/devtools", never], ["zustand/persist", unknown]],
+  [["zustand/devtools", never], ["zustand/persist", unknown]],
+  ListState & ListActions
+> = (set, get) => ({
+  ...initialState,
+  setParams: (newParams) => {
+    const { getCoffeeList, params } = get()
+    set({ params: { ...params, ...newParams } }, false, "setParams")
+    getCoffeeList(params)
+  },
+  getCoffeeList: async (params?: getCoffeeListReqParams) => {
+    try {
+      const { data } = await axios.get<CoffeeType[]>(BASE_URL, { params })
+      set({ coffeeList: data })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+})
